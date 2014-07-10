9f86c7c9b191091cc54178daf893a2d29ffc64f9
DropbearNinja <jhalewood@gmail.com>
2014-07-10 18:58:25 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import os, os.path
 import cherrypy
 
-class HelloWorld(object):
+class HTPConnect(object):
     @cherrypy.expose
     def index(self):
 	return file('index.html')
 
 
-cherrypy.quickstart(HelloWorld(), '/', 'server.conf')
+cherrypy.quickstart(HTPConnect(), '/', 'server.conf')
