41ec758cf00f54eabe5b5eb369e41dc9ed8305b3
davidpearce <davidpearce2@gmail.com>
2014-07-11 22:28:42 +0800

    added code to add/store settings and save to file

--- settings.py
+++ settings.py
@@ -1,37 +1,54 @@
 import os, os.path
 import cherrypy
 import json
+from cherrypy.lib.httputil import parse_query_string
 
 
-class Settings(object):
-    """ Main Class """
-
-    def set(self, key, val):
-    #do stuff
-        return true
-
-    def get(self,key, defval=''):
-    #do stuff
-        return true
+class Settings:
+    dictionary = {"settings": "deadffault"}
+    
+    def __init__(self):
+        self.readsettings()
     
     @cherrypy.expose
-    def readSettings(self):
+    def set(self, *args, **kwargs):
+        key = cherrypy.request.params.get("key")
+        value = cherrypy.request.params.get("value")
+
+        Settings.dictionary[key] = value
+
+    @cherrypy.expose
+    def get(self, *args, **kwargs):
+        key = cherrypy.request.params.get("key")
+
+        if key in Settings.dictionary:
+            return "Found " + key + " Value: " + Settings.dictionary[key]
+        
+        else:
+            return "Could not find key"
+    
+    @cherrypy.expose
+    def readsettings(self):
         #If settings doesnt exists, call write file with defaults
         if os.path.isfile('settings.ini'):
             with open('settings.ini', 'r') as infile:
+                #self.dictonary = json.load(infile)
+                #globalDict = data
                 data = json.load(infile)
-                return json.dumps(data)
+                
+                Settings.dictionary = data
+                return str(Settings.dictionary)
         else:
             self.writeDefaultSettings()
-            return 'fail'
+            self.readSettings()
 
     @cherrypy.expose
-    def writeSettings(self, data):
+    def writesettings(self):
         with open('settings.ini', 'w') as outfile:
-            json.dump(data, outfile)
+            json.dump(Settings.dictionary, outfile)
 
     @cherrypy.expose
-    def writeDefaultSettings(self):
+    def writedefaultsettings(self):
         data = { 'Settings': 'Default' }
         with open('settings.ini', 'w') as outfile:
             json.dump(data, outfile)
