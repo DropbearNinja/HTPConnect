0df838f9eb6b9a356b91257487f4481b2ba1102a
DropbearNinja <jhalewood@gmail.com>
2014-07-12 09:35:47 +0800

    minor changes

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import os, os.path
 import cherrypy
 import settings
+import sys
 
 class HTPConnect(object):
     @cherrypy.expose
     def index(self):
 	return file('index.html')
-
-
+	
+	def exit(self):  
+		cherrypy.engine.exit()
 
 
 cherrypy.quickstart(HTPConnect(), '/', 'server.conf')
