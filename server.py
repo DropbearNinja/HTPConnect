63c4314276e0949fc06c7d1caacd5d8abeb4dd1c
DropbearNinja <jhalewood@gmail.com>
2014-07-14 20:10:34 +0800

    removed spaces

--- server.py
+++ server.py
@@ -6,20 +6,20 @@ import sys
 import wsgiref.handlers
 
 class HTPConnect(object):
-    @cherrypy.expose
-    def index(self):
-	return file('index.html')
-    
-    @cherrypy.expose
-    def restart(self):
-        cherrypy.engine.restart()
+	@cherrypy.expose
+	def index(self):
+		return file('index.html')
+	
+	@cherrypy.expose
+	def restart(self):
+		cherrypy.engine.restart()
 
-    @cherrypy.expose
-    def exit(self):
-        cherrypy.engine.exit()
-		
+	@cherrypy.expose
+	def exit(self):
+		cherrypy.engine.exit()
+
 	@cherrypy.expose
 	def sickbeard(self):	
-		cherrypy.HTTPRedirect('http://192.168.0.53:8080')
+		raise cherrypy.HTTPRedirect('http://127.0.0.1:8081')
 		
 cherrypy.quickstart(HTPConnect(), '/', 'server.conf')
